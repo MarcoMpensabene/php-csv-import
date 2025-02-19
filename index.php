1bd125ae07e8a2a2f005bbf52c7365bda855d200
MarcoMpensabene <marcompensabene@gmail.com>
2025-02-19 15:15:03 +0100

    added a script for exporting the file too

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once 'database.php';
 echo "Connected successfully<br>";
 
-// Import CSV
+// Import CSV file 
 if (isset($_POST["import"]) && isset($_POST["import_type"])) {
     $importType = $_POST["import_type"];
 
@@ -32,4 +32,28 @@ if (isset($_POST["import"]) && isset($_POST["import_type"])) {
 
         fclose($file);
     }
+}
+// Export CSV file
+if (isset($_POST["export"])) {
+    $exportType = $_POST["export_type"];
+
+    header('Content-Type: text/csv; charset=utf-8');
+    header("Content-Disposition: attachment; filename={$exportType}.csv");
+
+    $output = fopen("php://output", "w");
+
+    if ($exportType == "products") {
+        fputcsv($output, ["id", "name", "price", "category_id"]);
+        $stmt = $conn->query("SELECT * FROM products");
+    } else {
+        fputcsv($output, ["id", "name"]);
+        $stmt = $conn->query("SELECT * FROM categories");
+    }
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        fputcsv($output, $row);
+    }
+
+    fclose($output);
+    exit;
 }
